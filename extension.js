da57dbfeb7442ae857a583a0069e884a5dbaebab
Dedar Alam <60034035+devded@users.noreply.github.com>
2025-05-09 20:59:58 +0600

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,14 +1,24 @@
 const vscode = require("vscode");
 
+/**
+ * @param {vscode.ExtensionContext} context
+ */
 function activate(context) {
-  const terminalItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+  const terminalItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
   terminalItem.text = `$(terminal) Terminal`;
-  terminalItem.tooltip = "Toggle Terminal";
-  terminalItem.command = "extension.toggleTerminal";
+  terminalItem.tooltip = "Click to toggle the terminal panel";
+  terminalItem.command = "bottomTerminal.toggle";
   terminalItem.show();
 
-  const disposable = vscode.commands.registerCommand("extension.toggleTerminal", () => {
-    vscode.commands.executeCommand("workbench.action.togglePanel");
+  const disposable = vscode.commands.registerCommand("bottomTerminal.toggle", async () => {
+    const activePanel = vscode.window.activeTerminal;
+
+    // Show or toggle terminal
+    if (!activePanel) {
+      vscode.commands.executeCommand("workbench.action.terminal.toggleTerminal");
+    } else {
+      vscode.commands.executeCommand("workbench.action.togglePanel");
+    }
   });
 
   context.subscriptions.push(terminalItem, disposable);
@@ -20,3 +30,4 @@ module.exports = {
   activate,
   deactivate,
 };
+
