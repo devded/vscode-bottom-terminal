2feff736d0e0311242ee60127a4fbfc6858492b8
Dedar Alam <60034035+devded@users.noreply.github.com>
2025-05-10 23:23:26 +0600

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,8 +1,11 @@
 const vscode = require("vscode");
 
 function activate(context) {
+  const config = vscode.workspace.getConfiguration("toggleTerminal");
+  const showLabel = config.get("showLabel", false); // default: icon only
+
   const terminalItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-  terminalItem.text = `$(terminal)`;
+  terminalItem.text = showLabel ? `$(terminal) Terminal` : `$(terminal)`;
   terminalItem.tooltip = "Toggle Terminal";
   terminalItem.command = "extension.toggleTerminal";
   terminalItem.show();
